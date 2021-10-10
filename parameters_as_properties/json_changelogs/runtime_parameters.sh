229c27de877c20962200537a0b95c8f6cc944862
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2021-10-09 22:23:57 -0500

    Update runtime_parameters.sh

--- parameters_as_properties/json_changelogs/runtime_parameters.sh
+++ parameters_as_properties/json_changelogs/runtime_parameters.sh
@@ -10,5 +10,5 @@ liquibase --defaultsFile=2st_set.properties updateSQL
 liquibase --defaultsFile=2st_set.properties update
 # running the a set of runtime parameters as "-Dparameter_name=parameter_value"
 # printing and confirming values with updateSQL
-liquibase --url=jdbc:h2:mem:liquibase --changeLogFile=test_variables1.json updateSQL -Dtable.name=myTable3 -DmySpecialValue=CHAR\(10\)
-liquibase --url=jdbc:h2:mem:liquibase --changeLogFile=test_variables1.json update -Dtable.name=myTable3 -DmySpecialValue=CHAR\(10\)
+liquibase --liquibase.hub.mode=off --url=jdbc:h2:mem:liquibase --changeLogFile=test_variables1.json updateSQL -Dtable.name=myTable3 -DmySpecialValue=CHAR\(10\)
+liquibase --liquibase.hub.mode=off --url=jdbc:h2:mem:liquibase --changeLogFile=test_variables1.json update -Dtable.name=myTable3 -DmySpecialValue=CHAR\(10\)
